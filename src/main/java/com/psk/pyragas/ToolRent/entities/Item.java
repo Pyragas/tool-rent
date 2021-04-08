0a5d1e8cdbe986934c40e7de0d1451dae40dc9b4
Gintautas Jurgelevičius <gintunas@gmail.com>
2021-04-08 22:44:25 +0300

    Add initial entities

---
+++ src/main/java/com/psk/pyragas/ToolRent/entities/Item.java
@@ -0,0 +1,20 @@
+package com.psk.pyragas.ToolRent.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
