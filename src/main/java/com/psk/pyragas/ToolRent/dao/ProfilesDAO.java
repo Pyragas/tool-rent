72a0d8e8638fd375fe9f2493c7245d8185fce488
Mantas <gramolis1@gmail.com>
2021-05-21 22:15:56 +0300

    Added optimistic locking handling for profile modifying.
    Problem - bad myProfile data after cancelling field update

--- src/main/java/com/psk/pyragas/ToolRent/dao/ProfilesDAO.java
+++ src/main/java/com/psk/pyragas/ToolRent/dao/ProfilesDAO.java
@@ -1,14 +1,18 @@
 package com.psk.pyragas.ToolRent.dao;
 
 import com.psk.pyragas.ToolRent.entities.Profile;
+import lombok.SneakyThrows;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @ApplicationScoped
 public class ProfilesDAO {
+
     @Inject
     private EntityManager em;
 
@@ -18,8 +22,14 @@ public class ProfilesDAO {
         return em.find(Profile.class, id);
     }
 
+
     public Profile update(Profile profile) {
-        return em.merge(profile);
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        Profile profile1 = em.merge(profile);
+        System.out.println("after merge");
+        externalContext.getSessionMap().put("user", profile);
+        System.out.println("after context update");
+        return profile1;
     }
 
     public List<Profile> loadAll() {
--- src/main/java/com/psk/pyragas/ToolRent/usecases/EditProfile.java
+++ src/main/java/com/psk/pyragas/ToolRent/usecases/EditProfile.java
@@ -4,16 +4,30 @@ import com.psk.pyragas.ToolRent.dao.ProfilesDAO;
 import com.psk.pyragas.ToolRent.entities.Profile;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
+import org.apache.commons.beanutils.BeanUtils;
+import org.primefaces.PrimeFaces;
+
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.inject.Model;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.OptimisticLockException;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.io.Serializable;
 
-@Model
-public class EditProfile {
+@ViewScoped
+@Named
+@Getter
+@Setter
+@Transactional
+public class EditProfile implements Serializable {
 
     ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 
@@ -29,9 +43,40 @@ public class EditProfile {
         profile = ((Profile) externalContext.getSessionMap().get("user"));
     }
 
-    @Transactional
-    public String updateProfile() {
-        profilesDAO.update(profile);
-        return "index.xhtml?faces-redirect=true";
+    public void updateProfile() {
+        try {
+            profilesDAO.update(profile);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml?faces-redirect=true");
+        }
+        catch (OptimisticLockException | IOException e) {
+            System.out.println("exception");
+            PrimeFaces.current().executeScript("PF('optimisticButton').jq.click()");
+        }
+
+    }
+
+    public void addMessage(FacesMessage.Severity messageType, String summary, String detail) {
+        FacesMessage message = new FacesMessage(messageType, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void refreshAndContinue() throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("edit_profile.xhtml?id=" + profile.getId() );
+        addMessage(FacesMessage.SEVERITY_INFO, "Data has been successfully updated.", "Refresh and update.");
+    }
+
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public void overwrite() throws IOException {
+        System.out.println("overwritten");
+        Profile profileInDB = profilesDAO.findOne(profile.getId());
+        try {
+            BeanUtils.copyProperties(profileInDB, profile);
+            profilesDAO.update(profileInDB);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("my_profile.xhtml");
+            addMessage(FacesMessage.SEVERITY_INFO, "Data has been overwritten.", "Overwritten.");
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("edit_profile.xhtml?id=" + profile.getId());
+            addMessage(FacesMessage.SEVERITY_INFO, "Sorry, unable to overwrite.", "Please edit again.");
+        }
     }
 }
