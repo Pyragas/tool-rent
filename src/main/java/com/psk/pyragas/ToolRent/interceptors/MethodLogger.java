5b7a85fc953ebeec933c965727fdabf533e91044
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-05-21 19:10:54 +0300

    Added placeholders for injected component that writes logger info to a file.

--- src/main/java/com/psk/pyragas/ToolRent/interceptors/MethodLogger.java
+++ src/main/java/com/psk/pyragas/ToolRent/interceptors/MethodLogger.java
@@ -11,13 +11,18 @@ import java.sql.Timestamp;
 public class MethodLogger implements Serializable {
     @AroundInvoke
     public Object logMethodInfo(InvocationContext context) throws Exception {
-        Object methodResult = context.proceed();
         System.out.println("--METHOD LOGGER START--");
-        System.out.println("Time: " + new Timestamp(System.currentTimeMillis()));
+        System.out.println("Time before method call: " + new Timestamp(System.currentTimeMillis()));
+        System.out.println("Entering method: " + context.getMethod().getName());
+        System.out.println("Entering class: " + context.getTarget().getClass());
+        //injectedComponentMethod();
+        Object methodResult = context.proceed();
+        System.out.println("Time after method call: " + new Timestamp(System.currentTimeMillis()));
         System.out.println("Called method: " + context.getMethod().getName());
         System.out.println("Caller class: " + context.getTarget().getClass());
         System.out.println("Method result: " + (methodResult == null ? "No Result (void)" : methodResult.toString()));
         System.out.println("--METHOD LOGGER END--");
+        //injectedComponentMethod();
         return methodResult;
     }
 }
