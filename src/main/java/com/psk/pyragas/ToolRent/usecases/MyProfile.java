237a0e3566cb009ad562e190e47015d58a5a45ad
Mantas <gramolis1@gmail.com>
2021-05-24 08:52:21 +0300

    Fixed myProfile not showing overwritten info

--- src/main/java/com/psk/pyragas/ToolRent/usecases/MyProfile.java
+++ src/main/java/com/psk/pyragas/ToolRent/usecases/MyProfile.java
@@ -1,5 +1,6 @@
 package com.psk.pyragas.ToolRent.usecases;
 
+import com.psk.pyragas.ToolRent.dao.ProfilesDAO;
 import com.psk.pyragas.ToolRent.entities.Profile;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 
 @Model
@@ -19,9 +21,12 @@ public class MyProfile {
     @Setter
     private Profile profile;
 
+    @Inject
+    private ProfilesDAO profilesDAO;
+
     @PostConstruct
     public void init() {
-        profile = ((Profile) externalContext.getSessionMap().get("user"));
+        profile = profilesDAO.findOne(((Profile) externalContext.getSessionMap().get("user")).getId());
     }
 
 }
