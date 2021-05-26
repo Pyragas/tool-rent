75edf8f28a7b751959897c3066b82089b7b81a9b
Gintautas Jurgelevičius <gintunas@gmail.com>
2021-05-26 18:55:52 +0300

    Update dialog to show normal text on button. Add user to interceptor.

--- src/main/java/com/psk/pyragas/ToolRent/utils/interceptors/MethodLogger.java
+++ src/main/java/com/psk/pyragas/ToolRent/utils/interceptors/MethodLogger.java
@@ -1,7 +1,10 @@
 package com.psk.pyragas.ToolRent.utils.interceptors;
 
+import com.psk.pyragas.ToolRent.entities.Profile;
 import com.psk.pyragas.ToolRent.utils.LoggerToFile;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
@@ -16,10 +19,14 @@ public class MethodLogger implements Serializable {
     @Inject
     private LoggerToFile logger;
 
+    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+
     @AroundInvoke
     public Object logMethodInfo(InvocationContext context) throws Exception {
         String logMessage = "";
         logMessage += "--METHOD LOGGER START--" + "\n";
+        Profile profile = (Profile)(externalContext.getSessionMap().get("user"));
+        logMessage += "Profile id: " + (profile==null ? null : profile.getId()) + "\n";
         logMessage += "Time before method call: " + new Timestamp(System.currentTimeMillis()) + "\n";
         logMessage += "Entering method: " + context.getMethod().getName() + "\n";
         logMessage += "Entering class: " + context.getTarget().getClass() + "\n";
